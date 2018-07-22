ed11054ae0d59e9efea16ef26cbe115fba996da7
KLIV <wclivingston5@gmail.com>
2018-07-21 22:16:51 -0500

    html js and css overhaul

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,24 +1,29 @@
+// Web Clock
+setInterval(function () {
+    $(".display-clock").html(moment().format('dddd, MMMM D YYYY, h:mm:ss A'))
+}, 1000);
+
 // Initialize Firebase
 var config = {
-    apiKey: "AIzaSyD0t-bfMCMLiGdxFLH4I8OmUo2fPRqcfLY",
-    authDomain: "train-station-96898.firebaseapp.com",
-    databaseURL: "https://train-station-96898.firebaseio.com",
-    projectId: "train-station-96898",
-    storageBucket: "train-station-96898.appspot.com",
-    messagingSenderId: "301137556358"
+    apiKey: "AIzaSyCVJwhVu1Id6T-1rl7HVRiTPPBj7TjzqYI",
+    authDomain: "better-train-station.firebaseapp.com",
+    databaseURL: "https://better-train-station.firebaseio.com",
+    projectId: "better-train-station",
+    storageBucket: "better-train-station.appspot.com",
+    messagingSenderId: "368518174599"
 };
-firebase.initializeApp(config);
 
+firebase.initializeApp(config);
 // Variables to reference the database.
 var database = firebase.database();
 
-// Initial Values
+
 var tName = "";
 var tDestination = "";
-var firstTime = "";
+var tTime = "";
 var tFrequency = "";
-var minAway = "";
-var nextT = "";
+var nextT;
+var tMoment = moment();
 
 console.log(moment().format("LL"));
 
@@ -29,87 +34,67 @@ $("#add-train").on("click", function (event) {
     // Grabbed values from text-boxes
     tName = $("#name-input").val().trim();
     tDestination = $("#destination-input").val().trim();
-    firstTime = $("#firstTime-input").val().trim();
+    tTime = moment($("#tTime-input").val().trim(), "HH:mm").format("X");
     tFrequency = $("#frequency-input").val().trim();
-    
-    // firstTime Time (pushed back 1 year to make sure it comes before current time)
-    var firstTimeConv = moment(firstTime, "HH:mm").subtract(1, "years");
-    console.log(firstTimeConv);
-    
-    // Current Time
-    var currentTime = moment();    
-    console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
-    
-    // Difference between the times
-    var differT = moment().diff(moment(firstTimeConv), "minutes");
-    console.log("DIFFERENCE IN TIME: " + differT);
 
-    // Time apart (remainder)
-    var tRemainder = differT % tFrequency;
-    console.log(tRemainder);
-
-    // Minute Until Train
-    minAway = tFrequency - tRemainder;
-    console.log("MINUTES TILL TRAIN: " + minAway);
-
-    // Next Train
-    nextT = moment().add(minAway, "minutes");
-    console.log("ARRIVAL TIME: " + moment(nextT).format("hh:mm"));        
-
-
+    tMoment = moment().format('X');
     // Code for "Pushing values in the database"
-    database.ref().push({
+    database.ref().child('train').push({
         tName: tName,
         tDestination: tDestination,
-        firstTime: firstTime,
+        tTime: tTime,
         tFrequency: tFrequency,
-        nextT: nextT,
-        minAway: minAway
+        currentT: tMoment,
     });
-
-    var tBody = $("tbody");
-    var tRow = $("<tr>");
-
-    var nameTd = $("<td>").text(tName);
-    nameTd.addClass("name-display");
-    var destinationTd = $("<td>").text(tDestination);
-    destinationTd.addClass("destination-display");
-    var firstTimeTd = $("<td>").text(firstTime);
-    firstTimeTd.addClass("firstTime-display");
-    var frequencyTd = $("<td>").text(tFrequency);
-    frequencyTd.addClass("frequency-display");
-    var nextTd = $("<td>").text(nextT);
-    nextTd.addClass("next-display");
-    var minAwayTd = $("<td>").text(minAway);
-    minAwayTd.addClass("minAway-display");
-
-    tRow.append(nameTd, destinationTd, firstTimeTd, frequencyTd, nextTd, minAwayTd);
-    tBody.append(tRow);
-
 });
 
-// Firebase watcher + initial loader HINT: .on("value")
-database.ref().on("value", function (snapshot) {
+// Update minutes away by triggering change in firebase children
+function updateTime() {
+    database.ref().child('train').once('value', function (snapshot) {
+        snapshot.forEach(function (childSnapshot) {
+            tMoment = moment().format('X');
+            database.ref('trains/' + childSnapshot.key).update({
+                currentT: tMoment,
+            })
+        })
+    });
+};
 
-    // Log everything that's coming out of snapshot
-    console.log(snapshot.val());
-    console.log(snapshot.val().tName);
-    console.log(snapshot.val().tDestination);
-    console.log(snapshot.val().firstTime);
-    console.log(snapshot.val().tFrequency);
-    console.log(snapshot.val().next);
-    console.log(snapshot.val().minAway);
+setInterval(updateTime, 30000);
 
+// Reference Firebase when page loads and train added
+database.ref().child('train').on('value', function (snapshot) {
+    $('tbody').empty();
 
-    // Change the HTML to reflect
-    $(".name-display").text(snapshot.val().tName);
-    $(".destination-display").text(snapshot.val().tDestination);
-    $(".firstTime-display").text(snapshot.val().firstTime);
-    $(".frequency-display").text(snapshot.val().tFrequency);
-    $(".next-display").text(snapshot.val().nextT);
-    $(".minAway-display").text(snapshot.val().minAway);
+    snapshot.forEach(function (childSnapshot) {
+        console.log(childSnapshot.val());
+        var tTrain = childSnapshot.val();
+        var tTimeConv = moment.unix(tTrain.tTime);
+        var tDiff = moment().diff(moment(tTimeConv, 'HH:mm'), 'minutes');
+        var tRemainder = tDiff % parseInt(tTrain.tFrequency);
+        var minAway = parseInt(tTrain.tFrequency) - tRemainder;
 
-    // Handle the errors
+        if (tDiff >= 0) {
+            nextT = null;
+            nextT = moment().add(minAway, 'minutes').format('hh:mm A');
+
+        } else {
+            nextT = null;
+            nextT = tTimeConv.format('hh:mm A');
+            minAway = Math.abs(tDiff - 1);
+        }
+
+        var tClass = childSnapshot.key;
+
+        $('tbody').append(
+            "<tr class=" + tClass + ">" +
+            "<td>" + tTrain.tName + "</td>" +
+            "<td>" + tTrain.tDestination + "</td>" +
+            "<td>" + tTrain.tFrequency + "</td>" +
+            "<td>" + nextT + "</td>" +
+            "<td>" + minAway + "</td>");
+    });
+
 }, function (errorObject) {
     console.log("Errors handled: " + errorObject.code);
 });
